4d0c6c0c3609f7ade120fdffa3fe6ac3724e726c
GalPeter24 <galpeti20083@gmail.com>
2026-01-03 15:52:09 +0100

    map_1.1

--- pixel-knight/scenes/Player.cs
+++ pixel-knight/scenes/Player.cs
@@ -3,83 +3,83 @@ using Godot;
 
 public partial class Player : CharacterBody2D
 {
-    [Export]private float MoveSpeed = 200f;
+	[Export]private float MoveSpeed = 200f;
 
-    private AnimatedSprite2D animatedsprite;
+	private AnimatedSprite2D animatedsprite;
 
 public bool HasKey= false;
-    public override void _Ready()
-    {
-        animatedsprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-    }
+	public override void _Ready()
+	{
+		animatedsprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+	}
 
-    private void HandleAnimation(Vector2 direction){
+	private void HandleAnimation(Vector2 direction){
 
-        if(direction == Vector2.Zero)
-        {
-            animatedsprite.Stop();
-            return;
-        }
+		if(direction == Vector2.Zero)
+		{
+			animatedsprite.Stop();
+			return;
+		}
 
-        string anim = "";
+		string anim = "";
 
-        if(direction.X !=0){
-            anim=direction.X>0 ? "walkright" : "walkleft";
-        }
-        else if(direction.Y != 0)
-        {
-            anim=direction.Y>0? "walkdown" : "walkup";
-        }
+		if(direction.X !=0){
+			anim=direction.X>0 ? "walkright" : "walkleft";
+		}
+		else if(direction.Y != 0)
+		{
+			anim=direction.Y>0? "walkdown" : "walkup";
+		}
 
-        if(animatedsprite.Animation != anim){
-            animatedsprite.Play(anim);
-        }
-    }
+		if(animatedsprite.Animation != anim){
+			animatedsprite.Play(anim);
+		}
+	}
 
-    private void GetInput()
-    {
-        Vector2 inputDirection = Vector2.Zero;
-        //jobbra
-        if (Input.IsActionPressed("ui_right"))
-        {
-            inputDirection.X += 1;
-        }
-        //balra
-        if (Input.IsActionPressed("ui_left"))
-        {
-            inputDirection.X -= 1;
-        }
-       
-        //fel
-        if (Input.IsActionPressed("ui_up"))
-        {
-            inputDirection.Y -= 1;
-        }
-       
+	private void GetInput()
+	{
+		Vector2 inputDirection = Vector2.Zero;
+		//jobbra
+		if (Input.IsActionPressed("ui_right"))
+		{
+			inputDirection.X += 1;
+		}
+		//balra
+		if (Input.IsActionPressed("ui_left"))
+		{
+			inputDirection.X -= 1;
+		}
+	   
+		//fel
+		if (Input.IsActionPressed("ui_up"))
+		{
+			inputDirection.Y -= 1;
+		}
+	   
 
-        //le
-        if (Input.IsActionPressed("ui_down"))
-        {
-            inputDirection.Y += 1;
-        }
+		//le
+		if (Input.IsActionPressed("ui_down"))
+		{
+			inputDirection.Y += 1;
+		}
 
-        inputDirection = inputDirection.Normalized();
-        
-        Velocity = inputDirection * MoveSpeed;
+		inputDirection = inputDirection.Normalized();
+		
+		Velocity = inputDirection * MoveSpeed;
 
-        HandleAnimation(inputDirection);
-    }
+		HandleAnimation(inputDirection);
+	}
 
 
 public void PickupKey(){
-    HasKey=true;
-    GD.Print("player obijektum");
+	HasKey=true;
+	GD.Print("player obijektum");
 }
 
-    public override void _PhysicsProcess(double delta)
-    {
-        GetInput();
-        MoveAndSlide();
-    }
+	public override void _PhysicsProcess(double delta)
+	{
+		GetInput();
+		MoveAndSlide();
+	}
 
 }
